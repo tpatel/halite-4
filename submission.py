8095317c39cda69a92504a0d7e4a53a9616ac34d
Thibaut Patel <thibaut.patel@gmail.com>
2020-06-17 10:49:24 +0200

    fix: first random submission

--- submission.py
+++ submission.py
@@ -1,10 +1,24 @@
+from kaggle_environments.envs.halite.helpers import *
+
 from random import choice
 
+turn = 0
 
-def agent(obs):
-    action = {}
-    ship_id = list(obs.players[obs.player][2].keys())[0]
-    ship_action = choice(["NORTH", "SOUTH", "EAST", "WEST", None])
-    if ship_action is not None:
-        action[ship_id] = ship_action
-    return action
+
+def agent(obs, config):
+    global turn
+    board = Board(obs, config)
+    me = board.current_player
+
+    if turn == 0:
+        me.ships[0].next_action = ShipAction.CONVERT
+    else:
+        for ship in me.ships:
+            ship.next_action = choice(ShipAction.moves() + [None])
+
+    for shipyard in me.shipyards:
+        if me.halite > 1000:
+            shipyard.next_action = ShipyardAction.SPAWN
+
+    turn = turn + 1
+    return me.next_actions
