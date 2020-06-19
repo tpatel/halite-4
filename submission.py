05cade4ece19a60fa4b66ade958c6bb80603febb
Thibaut Patel <thibaut.patel@gmail.com>
2020-06-19 10:57:57 +0200

    random search

--- submission.py
+++ submission.py
@@ -1,5 +1,5 @@
 from kaggle_environments.envs.halite.helpers import *
-
+import json
 from random import choice
 
 
@@ -10,7 +10,7 @@ def score(turn, board):
     me = board.current_player
 
     base = 1000 if len(me.shipyards) > 1 else 0
-    ships = len(me.ships) * 500
+    ships = len(me.ships) * 500 * (400-turn) / 400
     potential_halite = sum(map(lambda ship: ship.halite, me.ships)) * 0.1
     halite = me.halite * turn / 300
 
@@ -19,31 +19,62 @@ def score(turn, board):
 
 def randomMoves(turn, board):
     me = board.current_player
+    remaining_halite = me.halite
 
     if turn == 0:
         me.ships[0].next_action = ShipAction.CONVERT
     else:
         for ship in me.ships:
-            ship.next_action = choice(ShipAction.moves() + [None])
+            ship.next_action = choice(
+                ShipAction.moves() + [None, ShipAction.CONVERT])
 
     for shipyard in me.shipyards:
-        if me.halite > 1000:
-            shipyard.next_action = choice([ShipyardAction.SPAWN, None])
+        if len(me.ships) == 0 or remaining_halite > (1000 + turn*4):
+            shipyard.next_action = choice(
+                [ShipyardAction.SPAWN, None, None, None])
+            if shipyard.next_action == ShipyardAction.SPAWN:
+                remaining_halite -= 500
 
-    return me.next_actions.copy()
+    return board.next()
+
+
+def deepRandomMoves(depth, turn, board, actions):
+    if depth == 0:
+        return (score(turn, board), actions[0])
+
+    options = []
+    already_seen = set()
+
+    for _ in range(5):
+        next_board = randomMoves(turn, board)
+        next_actions = board.current_player.next_actions
+        id = json.dumps(next_actions)
+        # next_score = score(turn, board.next())
+        if id not in already_seen:
+            # if turn < 20:
+            #     print(str(score(turn, board)) + " " +
+            #           str(score(turn, next_board)))
+            #     print(deepRandomMoves(depth-1, turn+1, next_board,
+            #                           actions.copy() + [next_actions]))
+            options += [deepRandomMoves(depth-1, turn+1,
+                                        next_board, actions.copy() + [next_actions])]
+        already_seen.add(id)
+
+    # if turn < 20:
+    #     print(options)
+    options.sort(key=lambda x: x[0], reverse=True)
+
+    return options[0]
 
 
 def agent(obs, config):
     global turn
     board = Board(obs, config)
-    options = []
 
-    for _ in range(50):
-        next_actions = randomMoves(turn, board)
-        next_score = score(turn, board.next())
-        options += [(next_score, next_actions)]
+    if(turn < 10 or turn % 50 == 0):
+        print("turn " + str(turn))
 
-    options.sort(key=lambda x: x[0], reverse=True)
+    option = deepRandomMoves(2, turn, board, [])
 
     turn = turn + 1
-    return options[0][1]
+    return option[1]
